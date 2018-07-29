cd3bf249b7767c0d993d840ce410f44d5f29f189
Jacky <sla255@sfu.ca>
2018-07-28 20:50:23 -0700

    get hour, minute, second arm's postion and set their positon in clock

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+let hourPos = new Date().getHours() * 30;
+let minPos = new Date().getMinutes() * 6;
+let secPos= new Date().getSeconds() * 6;
+
+HOURHAND.setAttribute("style", "transform: rotate(" + hourPos +"deg)");
+MINUTEHAND.setAttribute("style", "transform: rotate(" + minPos +"deg)");
+SECONDHAND.setAttribute("style", "transform: rotate(" + secPos + "deg)");
