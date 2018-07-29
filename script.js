1cde9dbaccd669bd35475251c01e350ede361380
Jacky <sla255@sfu.ca>
2018-07-29 11:48:24 -0700

    make the clock running and solv the "return to zero" problem

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 // get Hour, Minute, Second arm pos use math
-let secPos= new Date().getSeconds() * 6;
-let minPos = (new Date().getMinutes() * 6) + secPos/60;
-let hourPos = (new Date().getHours() * 30) + minPos/12;
+var secPos= new Date().getSeconds() * 6;
+var minPos = (new Date().getMinutes() * 6) + secPos/60;
+var hourPos = (new Date().getHours() * 30) + minPos/12;
 
-HOURHAND.setAttribute("style", "transform: rotate(" + hourPos +"deg)");
-MINUTEHAND.setAttribute("style", "transform: rotate(" + minPos +"deg)");
-SECONDHAND.setAttribute("style", "transform: rotate(" + secPos + "deg)");
+function runClock(){
+    secPos = secPos + 6;
+    minPos = minPos + 1/10;
+    hourPos = hourPos + 1/120;
+
+    HOURHAND.setAttribute("style", "transform: rotate(" + hourPos +"deg)");
+    MINUTEHAND.setAttribute("style", "transform: rotate(" + minPos +"deg)");
+    SECONDHAND.setAttribute("style", "transform: rotate(" + secPos + "deg)");
+}
+
+setInterval( runClock, 1000);
