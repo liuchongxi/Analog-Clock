18b09b36611e598ddc219f12fd08913bf0e77044
Jacky <sla255@sfu.ca>
2018-07-29 11:24:50 -0700

    set hour, minute, second arms at relative posotion in the clock

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+// get Hour, Minute, Second arm
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hourPos = new Date().getHours() * 30;
-let minPos = new Date().getMinutes() * 6;
+// get Hour, Minute, Second arm pos use math
 let secPos= new Date().getSeconds() * 6;
+let minPos = (new Date().getMinutes() * 6) + secPos/60;
+let hourPos = (new Date().getHours() * 30) + minPos/12;
 
 HOURHAND.setAttribute("style", "transform: rotate(" + hourPos +"deg)");
 MINUTEHAND.setAttribute("style", "transform: rotate(" + minPos +"deg)");
